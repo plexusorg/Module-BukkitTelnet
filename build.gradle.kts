01caef2f755c93c050a7d7f86e0e53c10e8128cf
Telesphoreo <me@telesphoreo.me>
2022-08-02 20:42:18 -0500

    Update Gradle + remove API

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,9 +28,8 @@ repositories {
 dependencies {
     compileOnly("org.projectlombok:lombok:1.18.24")
     annotationProcessor("org.projectlombok:lombok:1.18.24")
-    compileOnly("io.papermc.paper:paper-api:1.19-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.19.1-R0.1-SNAPSHOT")
     compileOnly("dev.plex:server:1.2-SNAPSHOT")
-    compileOnly("dev.plex:api:1.2-SNAPSHOT")
     compileOnly("com.github.plexusorg:BukkitTelnet:6908ff201f") {
         exclude("org.papermc.paper", "paper-api")
     }
