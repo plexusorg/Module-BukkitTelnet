c6fcb1b58ede606bd517d26e8449b5425897069c
Telesphoreo <me@telesphoreo.me>
2022-11-27 22:17:15 -0600

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "dev.plex"
-version = "1.2-SNAPSHOT"
+version = "1.3-SNAPSHOT"
 description = "Module-BukkitTelnet"
 
 repositories {
@@ -28,8 +28,8 @@ repositories {
 dependencies {
     compileOnly("org.projectlombok:lombok:1.18.24")
     annotationProcessor("org.projectlombok:lombok:1.18.24")
-    compileOnly("io.papermc.paper:paper-api:1.19.1-R0.1-SNAPSHOT")
-    compileOnly("dev.plex:server:1.2-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.19.2-R0.1-SNAPSHOT")
+    compileOnly("dev.plex:server:1.3-SNAPSHOT")
     compileOnly("com.github.plexusorg:BukkitTelnet:6908ff201f") {
         exclude("org.papermc.paper", "paper-api")
     }
